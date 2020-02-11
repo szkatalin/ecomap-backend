a2c03acc103640812d0621e302c84929ce663a6f
Szalai Katalin <szkatalin@outlook.com>
2020-02-11 20:50:22 +0100

    set up database

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,12 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Get} from '@nestjs/common';
+import {AppService} from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor() {}
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  public root(): string {
+    return 'Backend is running! Hurray!!!!';
+  }
 }
