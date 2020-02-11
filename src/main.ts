0f5eeec6cb10bb6c0f490bb43c3755a7f5a34c78
Szalai Katalin <szkatalin@outlook.com>
2020-02-11 21:15:00 +0100

    add more initalization

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import * as env from "dotenv";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as bodyParser from 'body-parser';
+import 'reflect-metadata';
+env.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
   app.enableCors();
-  await app.listen(process.env.PORT || '80');
+
+  const port = process.env.PORT || '80';
+  console.log('App is running on port: ' + port);
+  await app.listen(port);
 }
 bootstrap();
