397a1ce78fbc4e88ebb5a9879c5a5825a579b066
szkatalin <szkatalin@outlook.com>
2021-01-22 13:47:11 +0100

    set cookies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import * as env from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// tslint:disable-next-line:no-implicit-dependencies
 import * as bodyParser from 'body-parser';
 import 'reflect-metadata';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger } from '@nestjs/common';
+import * as session from 'express-session';
 env.config();
 
 async function bootstrap() {
@@ -23,6 +25,11 @@ async function bootstrap() {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.enableCors({origin: true});
+
+  app.use(session({
+    secret: 'my-secret',
+    cookie: {sameSite: 'none', secure: true}
+  }));
 
   const port = process.env.PORT || '3000';
   logger.log('App is running on port: ' + port);
